906d0192bd991c32b3fdba1ecd2a247e157eb200
Eilin Seth <aksun989@gmail.com>
2025-04-07 21:43:36 +0700

    setup awal project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <Container
+        maxWidth="lg"
+        sx={{
+          backgroundColor: 'salmon',
+          minHeight: '100vh',
+          px: { xs: 2, sm: 3 }, 
+          py:"1%"
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        }}
+      >
+        <h1>Aku adalah singa</h1>
+        <p>Singa adalah salah satu hewan karnivora besar yang termasuk dalam keluarga kucing besar (Felidae). Mereka dikenal sebagai "raja hutan" karena kekuatan, keberanian, dan posisi mereka di puncak rantai makanan. Singa hidup berkelompok yang disebut kawanan, yang biasanya terdiri dari satu atau dua jantan dewasa, beberapa betina, dan anak-anak mereka. Singa jantan memiliki surai yang khas, yang menjadi simbol kekuatan dan status mereka.</p>
+        <p>Singa biasanya ditemukan di padang rumput dan sabana di Afrika, meskipun ada populasi kecil singa Asia yang hidup di Hutan Gir, India. Mereka adalah pemburu yang sangat terampil, dengan betina yang biasanya bertanggung jawab atas perburuan. Makanan utama mereka adalah hewan besar seperti zebra, rusa, dan kerbau. Singa juga memiliki suara auman yang sangat kuat, yang dapat terdengar hingga jarak 8 kilometer, digunakan untuk menandai wilayah dan berkomunikasi dengan anggota kawanan.</p>
+
+      
+      </Container>
     </div>
   );
 }
